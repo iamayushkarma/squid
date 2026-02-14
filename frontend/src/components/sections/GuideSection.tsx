430772e66c63ab7e07cbc6560a3f9b6b09d16014
Ayush Karma <ayushkarma.dev@gmail.com>
2026-02-14 23:13:46 +0530

    feat: add guide section

---
+++ frontend/src/components/sections/GuideSection.tsx
@@ -0,0 +1,39 @@
+import Orbit from "/svg/orbit.svg";
+import LightLogo from "/svg/light-logo.svg";
+import Mailchimp from "/svg/mailchimp.svg";
+import Studio from "/svg/studio.svg";
+import Gear from "/svg/gear.svg";
+import Point from "/svg/point.svg";
+import Button from "../ui/Button";
+
+function GuideSection() {
+  return (
+    <section className="w-4/5 relative mx-auto flex gap-5 mt-10 space-y-10">
+      {/* Orbit section */}
+      <div>
+        <img src={Orbit} />
+        <div>
+          <img className="absolute  top-78 left-[19.1rem]" src={LightLogo} />
+          <img className="absolute top-52 left-36" src={Gear} />
+          <img className="absolute top-92 left-[8.7rem]" src={Point} />
+          <img className="absolute top-118 left-56" src={Studio} />
+          <img className="absolute top-[67%] left-[39.5%]" src={Point} />
+          <img className="absolute top-[35%] left-[41.5%]" src={Mailchimp} />
+        </div>
+      </div>
+      {/* Content section */}
+      <div className="flex w-[27.6rem] flex-col items-start justify-center">
+        <h2 className="text-text-primary font-semibold text-5xl ">
+          We're here to guide and help you at all times
+        </h2>
+        <p className="text-text-secondary mt-8 text-[1.125rem]">
+          A good design is not only aesthetically pleasing, but also functional.
+          It should be able to solve the problem
+        </p>
+        <Button className="mt-8" text="Download" />
+      </div>
+    </section>
+  );
+}
+
+export default GuideSection;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import FeatureSection from "@/components/sections/FeatureSection";
 import GridMask from "@/components/sections/GridMask";
+import GuideSection from "@/components/sections/GuideSection";
 import HeroSection from "@/components/sections/HeroSection";
+
 function Home() {
   return (
     <div>
       <HeroSection />
       <GridMask />
       <FeatureSection />
+      <GuideSection />
     </div>
   );
 }
